0c47e6cacc1e50124e2aca71c0d11872996a9855
Yourgururajlande7 <gururajlande7@gmail.com>
2026-07-09 11:08:43 +0530

    update

--- 2d_arrays/73. Set Matrix Zeroes.cpp
+++ 2d_arrays/73. Set Matrix Zeroes.cpp
@@ -34,3 +34,57 @@ public:
 //Optimal 
 
 
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int row = matrix.size(), col = matrix[0].size();
+        bool row0 = 0, col0 = 0;
+        for (int i = 0; i < col; i++)
+        {
+            if (matrix[0][i] == 0)
+            {
+                row0 = true;
+                break;
+            }
+        }
+        for (int i = 0; i < row; i++)
+        {
+            if (matrix[i][0] == 0)
+            {
+                col0 = true;
+                break;
+            }
+        }
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 1; j < col; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 1; j < col; j++)
+            {
+                if (matrix[0][j] == 0 or matrix[i][0] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        if(row0)
+        {
+            for (int i = 0; i < col; i++)
+                matrix[0][i] = 0;
+        }
+        if(col0)
+        {
+            for (int i = 0; i < row; i++)
+                matrix[i][0] = 0;
+        }
+    }
+};
