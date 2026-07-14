3ba39605cc866931bd66497bfd0009ef31bb8c89
Yourgururajlande7 <gururajlande7@gmail.com>
2026-07-14 19:42:33 +0530

    update

---
+++ Binary_Search/875. Koko Eating Bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        int ans = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canEat(piles, mid, h)) {
+                ans = mid;       
+                right = mid - 1;
+            } else {
+                left = mid + 1;   
+            }
+        }
+        return ans;
+    }
+
+    bool canEat(vector<int>& piles, int speed, int h) {
+        long long hours = 0;
+        for (int bananas : piles) {
+            hours += (bananas + speed - 1) / speed; 
+        }
+        return hours <= h;
+    }
+};
